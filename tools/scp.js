eb1da598572b972d69081a9a83c34c2c66bf4b84
Shane Lupton <shanelupton@hurricaneprint.com>
2025-02-18 18:56:31 -0700

    Added ssh command back in

--- tools/scp.js
+++ tools/scp.js
@@ -1,10 +1,27 @@
 const {Client} = require('node-scp')
+const { NodeSSH } = require('node-ssh');
+const ssh = new NodeSSH();
 const path = require('path')
 const dirname = path.join(__dirname, '..')
 const {PRINTERIP, USER, PASSWORD} = require(dirname + '/network-config.js')
 const remoteFileDir = '/root/Filament-Sync-Service/data'
 const localDataDir = dirname + '/data'
 
+let runCommand = async () => {
+    try {
+        await ssh.connect({
+            host: PRINTERIP,
+            port: '22',
+            username: USER,
+            password: PASSWORD
+        });
+        await ssh.execCommand('sh /root/Filament-Sync-Service/service/sync.sh');
+    } catch (error) {
+        console.error('SSH connection or command execution error:', error);
+    } finally {
+        ssh.dispose();
+    }
+}
 
 const sendFiles = () => {
     Client({
@@ -16,8 +33,9 @@ const sendFiles = () => {
         client.uploadDir(localDataDir, remoteFileDir)
             .then(response => {
                 client.close() 
-            })
-            .catch(error => {})
+            }).then(() => {
+                runCommand()
+            }).catch(error => {})
     }).catch(e => console.log(e))
 }
 
