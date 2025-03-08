e4bfcedfa4664882d4a4fc1d9daff04adfd86fc5
Shane Lupton <shanelupton@hurricaneprint.com>
2025-03-07 21:37:07 -0700

    Updated to work with @jamincollins new root folder structure

--- tools/scp.js
+++ tools/scp.js
@@ -5,7 +5,9 @@ const path = require('path')
 const dirname = path.join(__dirname, '..')
 const {PRINTERIP, USER, PASSWORD} = require(dirname + '/network-config.js')
 const remoteFileDir = '/root/Filament-Sync-Service/data'
+const jaminFileDir = '/mnt/UDISK/root/Filament-Sync-Service/data'
 const localDataDir = dirname + '/data'
+
 
 let runCommand = async () => {
     try {
@@ -24,19 +26,29 @@ let runCommand = async () => {
 }
 
 const sendFiles = () => {
+    let remoteDir = ''
     Client({
         host: PRINTERIP,
         port: 22,
         username: USER,
         password: PASSWORD,
     }).then(client => {
-        client.uploadDir(localDataDir, remoteFileDir)
+        const result = client.exists(jaminFileDir)
+        .then(result => {
+            if (result) {
+                remoteDir = jaminFileDir
+            } else {
+                remoteDir = remoteFileDir
+            }
+        }).then(() => {
+            client.uploadDir(localDataDir, remoteDir)
             .then(response => {
                 client.close() 
             }).then(() => {
                 runCommand()
-            }).catch(error => {})
-    }).catch(e => console.log(e))
+            }).catch(error => console.log(error))
+        })
+    }).catch(error => console.log(error))
 }
 
 module.exports = {sendFiles}
