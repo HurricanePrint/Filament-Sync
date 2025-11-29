d7a612247bba36d575b7323b472e15febe0db538
Shane Lupton <shanelupton@hurricaneprint.com>
2025-11-29 02:40:06 -0600

    updated to new remote data path and removed unused node modules

--- tools/scp.js
+++ tools/scp.js
@@ -4,57 +4,25 @@ const ssh = new NodeSSH();
 const path = require('path')
 const dirname = path.join(__dirname, '..')
 const {PRINTERIP, USER, PASSWORD} = require(dirname + '/user-config.js')
-const remoteFileDir = '/root/Filament-Sync-Service/data'
-const jaminFileDir = '/mnt/UDISK/root/Filament-Sync-Service/data'
+const remoteDir = '/mnt/UDISK/printer_data/config/Filament-Sync-Service/data'
 const localDataDir = dirname + '/data'
 
-const checkDirectory = () => {
-    return new Promise((resolve,reject) => {
-        let remoteDir = ''
-        Client({
-            host: PRINTERIP,
-            port: 22,
-            username: USER,
-            password: PASSWORD,
-        }).then((client) => {
-            const result = client.exists(jaminFileDir)
-            .then(result => {
-                if (result != false) {
-                    remoteDir = jaminFileDir
-                } else {
-                    remoteDir = remoteFileDir
-                }
-            }).then(()=> {
-                client.close()
-                resolve(remoteDir)
-            })
+const sendFiles = () => {
+    Client({
+        host: PRINTERIP,
+        port: 22,
+        username: USER,
+        password: PASSWORD,
+    }).then(client => {
+        client.uploadDir(localDataDir, remoteDir)
+            .then(response => {
+                client.close() 
         }).catch(error => {
             console.error('\nCheck user-config.js to make sure printer info is set correctly\n')
             console.error(error)
         })
     })
-} 
-
-const sendFiles = () => {
-    let remoteDir = ''
-        checkDirectory().then((response) => {
-            remoteDir = response
-        }).then(() => {
-            Client({
-                host: PRINTERIP,
-                port: 22,
-                username: USER,
-                password: PASSWORD,
-            }).then(client => {
-                client.uploadDir(localDataDir, remoteDir)
-                    .then(response => {
-                        client.close() 
-            }).catch(error => {
-                console.error('\nCheck user-config.js to make sure printer info is set correctly\n')
-                console.error(error)
-            })
-        })
-    })
 }
+sendFiles()
  
 module.exports = {sendFiles}
