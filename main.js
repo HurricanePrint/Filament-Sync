97b872d6edeb656f455629949e22a66948a80ca0
HurricanePrint <shanelupton@hurricaneprint.com>
2026-01-17 17:50:35 -0600

    switched to scp and added service installer

--- main.js
+++ main.js
@@ -2,11 +2,19 @@ const addOptions = require('./tools/options-tool.js')
 const addProfiles = require ('./tools/database-tool.js')
 const {initData} = require('./tools/config.js')
 const sendToPrinter = require('./tools/scp.js')
+const installService = require('./tools/service-installer.js')
 
-initData()
+const main = async () => {
+    try {
+        await installService()
+        initData()
+        addOptions()
+        addProfiles()
+        sendToPrinter()
+    } catch (err) {
+        console.error(err)
+    }
 
-addOptions()
+}
 
-addProfiles()
-
-sendToPrinter()
+main()
